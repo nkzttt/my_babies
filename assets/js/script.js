fec0b4ba9111dbca469340a58ce2e2373e2634dc
nkzttt <free.nkzt@gmail.com>
2020-03-14 17:47:21 +0900

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@
   $loading.children(':last').on('animationend', function () {
     if (isLoaded) {
       $loading.fadeOut(300);
+      // start load web font
+      $('head').append('<link rel="stylesheet" href="/my_babies/assets/css/fonts.css">');
       return;
     }
     $loading.children().each(function () {

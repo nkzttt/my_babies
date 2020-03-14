4cc86aac46d3495136ab918ad632be5b8f7a6df7
nkzttt <free.nkzt@gmail.com>
2020-03-14 18:09:12 +0900

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 // loading scope
 (function loading() {
+  $(function () {
+    // github からの web font 提供は遅すぎるので遅延させる...
+    $('head').append('<link rel="stylesheet" href="/my_babies/assets/css/fonts.css">');
+  });
+
   var isLoaded = false;
   var $loading = $('.loading');
   $(window).on('load', function () {
@@ -8,8 +13,6 @@
   $loading.children(':last').on('animationend', function () {
     if (isLoaded) {
       $loading.fadeOut(300);
-      // start load web font
-      $('head').append('<link rel="stylesheet" href="/my_babies/assets/css/fonts.css">');
       return;
     }
     $loading.children().each(function () {

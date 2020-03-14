7555d5b645d33de27391c368366ea0aac4147c12
nkzttt <free.nkzt@gmail.com>
2020-03-14 17:54:38 +0900

    make to load lazy web font.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@
   var $loading = $('.loading');
   $(window).on('load', function () {
     isLoaded = true;
+    // github からの web font 提供は遅すぎるので遅延させる...
+    $('head').append('<link rel="stylesheet" href="/my_babies/assets/css/fonts.css">');
   });
   $loading.children(':last').on('animationend', function () {
     if (isLoaded) {

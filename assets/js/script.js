62c683b5f611e910175c3338660431d5b6968349
nkzttt <free.nkzt@gmail.com>
2020-03-15 10:25:21 +0900

    hid loading if not production.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // loading scope
 (function loading() {
   var isLoaded = false;
-  var $loading = $('.loading');
   $(window).on('load', function () {
     isLoaded = true;
     // github からの web font 提供は遅すぎるので遅延させる...
     $('head').append('<link rel="stylesheet" href="/my_babies/assets/css/fonts.css">');
   });
+  var $loading = $('.loading');
+  if ($loading.get().length === 0) return; // for development
   $loading.children(':last').on('animationend', function () {
     if (isLoaded) {
       $loading.fadeOut(300);

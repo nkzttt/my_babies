8a47460817d05c572ad99621edc64f7d9722c5ff
nkzttt <free.nkzt@gmail.com>
2020-04-17 15:48:10 +0900

    Updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,6 @@
     isLoaded = true;
     // github からの web font 提供は遅すぎるので遅延させる...
     $('head').append('<link rel="stylesheet" href="/my_babies/assets/css/fonts.css">');
-    // adsense も loading からは除外
-    $('body').append('<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>');
   });
   var $loading = $('.loading');
   if ($loading.get().length === 0) return; // for development
